714c6e6135b89a86b953e72151e58bc812d531ef
Paul Solt <PaulSolt@gmail.com>
2020-04-13 12:06:17 -0400

    Added starter mac project

---
+++ iOS14-Objective-C-Fundamentals/iOS14-Objective-C-Fundamentals/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS14-Objective-C-Fundamentals
+//
+//  Created by Paul Solt on 4/13/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
